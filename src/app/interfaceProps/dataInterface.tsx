d230ab2a985085b5abacdaff497e77621b3176ea
Aditya <aditya.prajapati@succssive.tech>
2024-12-12 12:42:11 +0530

    checkoutData

--- src/app/interfaceProps/dataInterface.tsx
+++ src/app/interfaceProps/dataInterface.tsx
@@ -6,10 +6,23 @@ export interface CheckoutData {
     dietary: string[];
     allergies: string[];
     allergyNotes: string;
-    snackProtein: number; 
-    proteinSmoothy: number; 
-    juice: number; 
-    coffee: number; 
+
+    snackProtein: number;
+    snackPrice: number;
+    snackPlanId: string;
+
+    proteinSmoothy: number;
+    proteinSmoothyPrice: number;
+    proteinSmoothyPlanId: string;
+
+    juice: number;
+    juicePrice: number;
+    juicePlanId: string;
+
+    coffee: number;
+    coffeePrice: number;
+    coffeePlanId: string;
+
     email: string;
     zipCode: string;
     firstName: string;
@@ -21,9 +34,41 @@ export interface CheckoutData {
     state: string;
     deliveryNotes: string;
     firstDeliveryDate: string;
+
     billingAddress: string;
     billingSuite: string;
     billingCity: string;
     billingState: string;
     billingZipCode: string;
+
+    planId: string;
+    stripeToken: string;
+    invitationCode: string;
+    invitationReferralCode: string;
+
+    planAmount: number;
+    carbs: string;
+    protein: string;
+    fat: string;
+    googleAddress: string;
+
+    marketingSource: string[];
+    samecheck: string;
+    isAddressSameCheckByUser: boolean;
+    acceptTnc: boolean;
+
+    couponCode: string;
+    couponValue: number;
+    remainingCreditBalance: number;
+    subTotalAmount: number;
+    totalAmount: number;
+    dailyDeliveryFee: number;
+    deliveryAmount: number;
+    discountCouponAmount: number;
+    discountCouponName: string;
+    discountCouponType: string;
+    isCouponApplied: boolean;
+    totalAmountAfterDiscount: number;
+
+    cityNameByZipCode: string | null;
 }
